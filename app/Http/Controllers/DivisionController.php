97b269974c4548638f836de2d538218e0c6c61da
Carol28 <carol.vi28@gmail.com>
2023-12-10 18:39:24 -0500

    divisiones

--- app/Http/Controllers/DivisionController.php
+++ app/Http/Controllers/DivisionController.php
@@ -32,14 +32,25 @@ class DivisionController extends Controller
 
     public function actualizarDivisiones(Request $request)
     {
-        // Validar y actualizar la división
-        $request->validate(Division::rules($request->id));
-
-        $division = Division::findOrFail($request->id);
-        $division->update($request->all());
-
-        return response()->json(['message' => 'División actualizada correctamente', 'division' => $division]);
+        try {
+            // Validar y actualizar la división
+            $request->validate(Division::rules($request->id));
+    
+            $division = Division::findOrFail($request->id);
+            $division->update($request->all());
+    
+            return response()->json(['message' => 'División actualizada correctamente', 'division' => $division]);
+        } catch (\Illuminate\Validation\ValidationException $validationException) {
+            // Capturar excepción de validación y obtener mensajes de error
+            $errors = $validationException->errors();
+    
+            return response()->json(['error' => 'Error de validación', 'messages' => $errors], 422);
+        } catch (\Exception $e) {
+            // Capturar otras excepciones
+            return response()->json(['error' => 'Error al procesar la solicitud. Detalles: ' . $e->getMessage()], 500);
+        }
     }
+    
 
     public function listarDivisiones(Request $request)
     {
